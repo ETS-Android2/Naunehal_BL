a32169538cd2541e6c31aa6d05ec938d6573f5f5
SAJID LATIF <indus1530@gmail.com>
2021-02-02 17:45:55 +0500

    Section082SEActivity skipSE36 modified

--- app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section082SEActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/naunehal/ui/sections/Section082SEActivity.java
@@ -45,13 +45,11 @@ public class Section082SEActivity extends AppCompatActivity implements EndSectio
         });
 
         bi.se36.setOnCheckedChangeListener(((radioGroup, i) -> {
-            bi.fldGrpse36.setVisibility(View.VISIBLE);
-            bi.fldGrpse37.setVisibility(View.VISIBLE);
-            if (i == bi.se3602.getId() || i == bi.se3698.getId()) {
-                Clear.clearAllFields(bi.fldGrpse36);
-                Clear.clearAllFields(bi.fldGrpse37);
-                bi.fldGrpse36.setVisibility(View.GONE);
-                bi.fldGrpse37.setVisibility(View.GONE);
+            if (i == bi.se3601.getId()) {
+                bi.llse36.setVisibility(View.VISIBLE);
+            } else {
+                Clear.clearAllFields(bi.llse36);
+                bi.llse36.setVisibility(View.GONE);
             }
         }));
     }
